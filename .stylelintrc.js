6663c45f5a48edd82157ce75afcd7ae792819dac
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2024-02-05 22:34:11 +0200

    first iteration of min page

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
-  extends: [
-    "stylelint-config-recommended-scss",
-    "@mate-academy/stylelint-config"
-  ],
-  plugins: [
-    "stylelint-scss"
-  ],
-  rules: {}
+  extends: "@mate-academy/stylelint-config",
+  plugins: ["stylelint-scss"],
+  rules: {},
 };
